1b17e132a4a577be839e994ec753f857101c115a
Siamion Chiper Petrenko <chiper.email@gmail.com>
2020-05-12 15:54:46 +0300

    Remove NetFramework solution

--- Net/CodeBattleNetLibrary/GameModels/Border.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeBattleNetLibrary.GameModels
-{
-    public class Border : Point
-    {
-        public Border(int x, int y) : base(x, y)
-        {
-        }
-    }
-}
--- Net/CodeBattleNetLibrary/Models/StepCommands.cs
+++
@@ -1,8 +0,0 @@
-namespace CodeBattleNetLibrary.Models
-{
-    public class StepCommands
-    {
-        public Commands CommandOne { get; set; } 
-        public Commands CommandTwo { get; set; }
-    }
-}
